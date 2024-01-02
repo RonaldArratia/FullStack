65e342e1432102b261291b3d130c512e74955813
RonaldArratia <ronaldarratia.999@gmail.com>
2024-01-02 11:44:29 -0400

    Curso React

---
+++ Practica_1/src/PrimerComponente.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const PrimerComponente = () => {
+  return (
+   <h1>Hola Mundo</h1>
+  )
+}
---
+++ Practica_1/src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { PrimerComponente } from "./primerComponente";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PrimerComponente />
+  </React.StrictMode>
+);
